5e3514b0674bfa1320b462caa3fd6d0da80c9553
fty57 <fty.57@alu.ufc.br>
2023-03-03 10:18:49 -0300

    120. Conhecendo melhor o styled-components

--- portfolio-frontend/src/templates/App/index.jsx
+++ portfolio-frontend/src/templates/App/index.jsx
@@ -1,10 +1,46 @@
 import { Wrapper } from './styles';
+import styled, { css } from 'styled-components';
+import { theme } from '../../styles/theme';
+
+const Heading = styled.h1`
+  color: purple;
+`;
+
+const Container = styled.div`
+  background: purple;
+
+  ${({ theme, bg }) => css`
+    color: ${theme.colors.secondaryBg};
+    background: ${bg};
+  `}
+
+  ${Heading} {
+    color: yellowgreen;
+  }
+`;
+
+const Container2 = styled(Container).attrs({ as: 'article' })`
+  background: blue;
+
+  ${Heading}:hover {
+    color: #c77b34;
+  }
+
+  &:hover {
+    background: pink;
+  }
+`;
 
 function Home() {
   return (
     <div className="App">
       <Wrapper>
-        <h1>Hello</h1>
+        <Container bg="red">
+          <Heading>Hello</Heading>
+        </Container>
+        <Container2>
+          <Heading>Hello</Heading>
+        </Container2>
       </Wrapper>
     </div>
   );
