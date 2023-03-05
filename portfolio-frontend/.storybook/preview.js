56b50a0ce50cc47380775d66000b600ea0445f1b
fty57 <fty.57@alu.ufc.br>
2023-03-05 10:07:30 -0300

    123. Configurando o Storybook - Parte 2

--- portfolio-frontend/.storybook/preview.js
+++ portfolio-frontend/.storybook/preview.js
@@ -4,6 +4,18 @@ import { theme } from '../src/styles/theme';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'light', values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primaryColor,
+      }
+    ]
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
